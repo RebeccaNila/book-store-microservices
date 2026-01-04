87e18fc57df422123e3a0b2eec392e8c2f204025
DESKTOP-86R2H8C\nila <nilarwin@cbbank.com.mm>
2026-01-04 17:35:00 +0630

    Update catalog-service v4 and add log on CatalogServiceApplication

--- catalog-service/src/main/java/dev/nila/bookstore/catalogservice/CatalogServiceApplication.java
+++ catalog-service/src/main/java/dev/nila/bookstore/catalogservice/CatalogServiceApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(CatalogServiceApplication.class, args);
+        System.out.println("It's running");
     }
 }
